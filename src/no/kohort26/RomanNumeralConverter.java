7b1a106982bbc37bb5e2585fd909405de79024c7
crashable <31343152+crashable@users.noreply.github.com>
2020-08-19 20:53:41 +0200

    La til forsøk på forklaringn av koden

--- src/no/kohort26/RomanNumeralConverter.java
+++ src/no/kohort26/RomanNumeralConverter.java
@@ -16,17 +16,18 @@ public class RomanNumeralConverter {
     private static final String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
     private static final String [] ones = {"", "I", "II", "III" ,"IV", "V", "VI", "VII", "VIII", "IX"};
 
-    public final static String toRoman(int number) {
-        String th = thousands[number / 1000];
-        String hu = hundreds[(number % 1000) / 100];
-        String te = tens[(number % 100) / 10];
-        String on = ones[number % 10];
-        return th + hu + te + on;
+    public final static String toRoman(int number) { // Kommentarer beskriver resultatet gitt argumentet: 1999
+        String th = thousands[number / 1000]; // her blir indexen tallet som er nærmest mulige index, som er 9,9 ( 1999 returnerer M her )
+        String hu = hundreds[(number % 1000) / 100]; // ( Her blir det 9,99, som da blir CM )
+        String te = tens[(number % 100) / 10]; // Blir 9,9 så XC
+        String on = ones[number % 10]; // 9 så IX
+        return th + hu + te + on; // Da blir svaret MCMXCIX
     }
 
     @Test
     void shouldConvert1ToI() {
         assertEquals("I", toRoman(1));
+        System.out.println("1999 er " + toRoman(1999));
     }
     @Test
     void shouldConvert2ToII() {
